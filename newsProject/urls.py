a1aa3f71bb3c0fed87b529881b85460479b4995b
ProfHezzy <hezekiahonline94@gmail.com>
2025-05-28 20:33:48 +0100

    Fixed merge conflict Projects files

--- newsProject/urls.py
+++ newsProject/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -8,14 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newsApp.urls'))
 ] + static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-=======
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('newsApp.urls'))
-] + static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
->>>>>>> 784256ef5fbcc3d56a7a2831162e02964209c51b
